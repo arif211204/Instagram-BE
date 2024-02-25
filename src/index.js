ffb4e1b9af2f3afa9dc0c5eec3f42e0f5e0ab644
arif123 <nashrullah2344@gmail.com>
2024-02-25 23:29:28 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -153,9 +153,9 @@ const pool = mysql.createPool({
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  dialect:process.env.MYSQL_DIALECT
+  database: process.env.MYSQL_DATABASE
 });
+
 
 
 app.use((req, res, next) => {
